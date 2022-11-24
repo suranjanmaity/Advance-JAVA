a612d5bc5ea9af7968bc57317d12289416d95884
suranjanmaity <suranjanmaity4@gmail.com>
2022-11-24 19:18:17 +0530

    connected to the database

--- jdbc-simple-project/src/com/qsp/Employee.java
+++ jdbc-simple-project/src/com/qsp/Employee.java
@@ -3,26 +3,47 @@ package com.qsp;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Employee {
 
     public static void main(String[] args) {
         
-        //step 1 load the driver
+        
+        Connection conn = null;
         try {
+            //step 1 load the driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             
             // step 2 establish the commection
-            String url = "jdbc:mysql://localhost:3306/";
+            String url = "jdbc:mysql://localhost:3306/jdbc-simple-project";
             String user = "maity";
             String pass = "1Q2w3e4r@";
 
-            Connection conn = DriverManager.getConnection(url, user, pass);
+            conn = DriverManager.getConnection(url, user, pass);
+
+            // step 3 create the statement
+            Statement statement = conn.createStatement();
+
+            // step 4 execute query
+            String insert = "insert into employee values(1000, 'ram', 'ram@gmail.com')";
+            statement.execute(insert);
+            System.out.println("inserted...");
+
+
 
             System.out.println(conn);
         } catch (ClassNotFoundException | SQLException e) {
-            // TODO: handle exception
             e.printStackTrace();
+        }
+
+        finally{
+            // step 5 close the connection
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         
         
